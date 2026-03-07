b0f8d5a968160f5f2d9e2aeb4d19b37d4582d653
laisuk <laisuk@yahoo.com>
2026-03-07 20:36:22 +0800

    Added Fluent 2 icons

--- src/i18n/initUiLanguage.ts
+++ src/i18n/initUiLanguage.ts
@@ -1,14 +1,13 @@
 // src/i18n/initUiLanguage.ts
 
-import { mustGetEl } from "../dom/refs";
-import { applyLocale } from "./applyLocale";
-import { getUiLanguage, setUiLanguage, UiLanguage } from "./index";
+import {mustGetEl} from "../dom/refs";
+import {applyLocale} from "./applyLocale";
+import {getUiLanguage, setUiLanguage, UiLanguage} from "./index";
 
 export function initUiLanguage(): void {
     const selectUiLanguage = mustGetEl<HTMLSelectElement>("select-ui-language");
 
-    const lang = getUiLanguage();
-    selectUiLanguage.value = lang;
+    selectUiLanguage.value = getUiLanguage();
 
     applyLocale();
 
