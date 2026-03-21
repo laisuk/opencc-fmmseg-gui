14c074e26e0a52ade1cc63ae0b6622b520205c72
laisuk <laisuk@yahoo.com>
2026-03-21 10:02:50 +0800

    Update opencc-fmmseg to v0.9.1

--- src/i18n/locales/zh-Hant.ts
+++ src/i18n/locales/zh-Hant.ts
@@ -12,7 +12,7 @@ export const zhHant = {
         t2s: "zh-Hant（正體） to zh-Hans（簡體）",
         manual: "Manual（自定義）",
         standard: "General（通用簡繁）",
-        zhTw: "ZH-TW（中台簡正）",
+        zhTw: "ZH-TW（中台簡繁）",
         zhHk: "ZH-HK（中港簡繁）",
         zhTwIdioms: "ZH-TW Idioms（中台慣用語）",
         punctuation: "Punctuation（標點）",
